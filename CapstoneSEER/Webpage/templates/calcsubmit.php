a4f61555586f5ba9c4d523a8259a3f2fd45dc3c5
Bret Gaulin <bret.gaulin@gmail.com>
2015-12-07 12:30:43 -0500

    Added PHP to form submit.
    Signed-off-by: Bret Gaulin <bret.gaulin@gmail.com>

---
+++ CapstoneSEER/Webpage/templates/calcsubmit.php
@@ -0,0 +1,14 @@
+<?php
+  if($_POST['calculate'] == "Submit")
+  {
+    $varAge = $_POST['age'];
+    $varRadtn = $_POST['radtn'];
+    $varRace = $_POST['race'];
+    $varLaterality = $_POST['laterality'];
+    $varTumorbehavior = $_POST['tumorbehavior'];
+    $varTumorstage = $_POST['tumorstage'];
+    $varNumberprims = $_POST['numberprims'];
+    $varErstatus = $_POST['erstatus'];
+    $varPrstatus = $_POST['prstatus'];
+  }
+  ?>
