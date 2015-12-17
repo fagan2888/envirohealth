468d356c5c9a7b84f132ce45cfada219b00e7ac1
bretsbk <bret.gaulin@gmail.com>
2015-12-16 19:39:29 -0500

    Updated webpages. Image display working. Last step to clean up formatting.
    Signed-off-by: bretsbk <bret.gaulin@gmail.com>

--- CapstoneSEER/Webpage/app.py
+++ CapstoneSEER/Webpage/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, g, json, request
-from ProjectSeer1 import ProjectSeer1
+﻿from flask import Flask, render_template, g, json, request
+import MasterSeer
 import sqlite3
+from ProjectSeer1 import ProjectSeer1
+import numpy as np
+import matplotlib.pyplot as plt
+import patsy as pt
+from lifelines import AalenAdditiveFitter
 
 app = Flask(__name__)
 
@@ -27,7 +32,8 @@ def showResults():
 
 @app.route("/showResultsTEST", methods=['POST'])
 def showResultsTEST():
-    _age = request.form['age']
+    _agedx = request.form['agedx']
+    _yrbrth = request.form['yrbrth']
     _radtn = request.form['radtn']
     _race = request.form['race']
     _laterality = request.form['laterality']
@@ -36,60 +42,11 @@ def showResultsTEST():
     _numprims = request.form['numberprims']
     _erstatus = request.form['erstatus']
     _prstatus = request.form['prstatus']
-    # print(_age, _radtn, _race, _laterality, _tumorbehavior, _tumorstage, _numprims, _erstatus, _prstatus)
-    user_vars_list = [_age, _radtn, _race, _laterality, _tumorbehavior, _tumorstage, _numprims, _erstatus, _prstatus]
-    user_vars_dict = {'AGE_DX': _age, 'RADIATN' : _radtn, 'RACE' : _race, 'LATERAL' : _laterality, 'BEHANAL' : _tumorbehavior, 'HST_STGA' : _tumorstage, 'NUMPRIMS' : _numprims, 'ERSTATUS' : _erstatus, 'PRSTATUS' : _prstatus}
-    for var in user_vars_list:
-        print(var)
-    for key,value in user_vars_dict.items():
-        print(key, value)
-    return render_template("results.html")
 
-    
-# @app.route("/showResults")
-# def showResults():
-#     return render_template("results.html")
-
-# @app.route("/showResults",methods=['GET'])
-# def calculate():
-#     return render_template('results.html')
-#     _age = request.form['age']
-#     _radtn = request.form['radtn']
-#     _race = request.form['race']
-#     _laterality = request.form['laterality']
-#     _tumorbehavior = request.form['tumorbehavior']
-#     _tumorstage = request.form['tumorstage']
-#     _numprims = request.form['numberprims']
-#     _erstatus = request.form['erstatus']
-#     _prstatus = request.form['prstatus']
-#
-#     fullfields = _age and _radtn and _race and _laterality and _tumorbehavior and _tumorstage and _numprims and _erstatus and _prstatus
-
-    # if fullfields:
-    #     return json.dumps({'html':'<span>OK</span>'})
-    # else:
-    #     return json.dumps({'html':'<span>Err</span>'})
-
-# Run http://localhost:5000/#
-
-# """
-# Database code
-# """
-# DATABASE = '../data/seer.db'
-#
-# def get_db():
-#     db = getattr(g, '_database', None)
-#     if db is None:
-#         db = g._database = connect_to_database()
-#     return db
-#
-# @app.teardown_appcontext
-# def close_connection(exception):
-#     db = getattr(g,'_database', None)
-#     if db is not None:
-#         db.close()
-
-# @app.route('TestQuery')
+    test1 = np.array([[1.,_yrbrth,_agedx,_radtn,_tumorstage,_erstatus,_prstatus,_tumorbehavior,_tumorstage,_numprims,_race]], dtype=np.float64)
+    seer = ProjectSeer1(sample_size = 5000, verbose=True)
+    res = seer.process_patient(test1)
+    return render_template("results.html",age = _agedx, birthyear = _yrbrth, radiation = _radtn,race = _race,laterality = _laterality,tumorbehavior = _tumorbehavior,tumorstage = _tumorstage,numprims = _numprims,erstatus = _erstatus,prstatus = _prstatus)
 
 if __name__ == "__main__":
     app.run()
